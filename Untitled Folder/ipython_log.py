4a3c84509fa77bcb540509904143052d54e2eb36
Sanghyuk <saanghyuk@gmail.com>
2018-07-20 03:24:03 +0900

    7/19

---
+++ Untitled Folder/ipython_log.py
@@ -0,0 +1,28 @@
+# IPython log file
+
+get_ipython().run_line_magic('paste', '')
+get_ipython().run_line_magic('cpaste', '')
+get_ipython().run_line_magic('pinfo', '%reset')
+get_ipython().run_line_magic('quickref', '')
+-
+_
+__
+_4
+_i4
+hi
+"hi"
+_i11
+print("hi")
+_i11
+exec _i11
+exec _i13
+exec _i13
+print("hi")
+exec _i18
+i18
+_12
+_i12
+get_ipython().run_line_magic('hist', '')
+get_ipython().run_line_magic('logstart', '')
+get_ipython().run_line_magic('logon', '')
+get_ipython().run_line_magic('logoff', '')
